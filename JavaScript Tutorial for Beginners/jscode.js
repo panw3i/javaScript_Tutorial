c42134194e6c37256b606a2d40b4ce2532b5bc0f
panw3i <panw3i@gmail.com>
2017-01-22 18:06:31 +0800

    Functions

---
+++ JavaScript Tutorial for Beginners/jscode.js
@@ -0,0 +1,19 @@
+/**
+ * Created by pan on 2017/1/22.
+ */
+function saySomething() {
+    // all statements go inside curly braces
+
+    console.log('This is a function')
+
+}
+
+function addSomething(num,str) {
+    // all statements go inside curly braces
+    var add = num + str;
+    console.log(add)
+
+}
+
+saySomething();
+addSomething(455,"Programes to write");
