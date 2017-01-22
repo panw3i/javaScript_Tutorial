13167461a07a1f5cd3ff8a4285e7a95ff3dc08e7
panw3i <panw3i@gmail.com>
2017-01-22 18:14:17 +0800

    Operations

---
+++ JavaScript Tutorial for Beginners/operators.js
@@ -0,0 +1,16 @@
+/**
+ * Created by pan on 2017/1/22.
+ */
+
+var number1 =7;
+var number2 = 4;
+// var number1 = number1+number2;
+
+number1+=number2;
+number1++;
+var number3 =  4 + 6*3;
+var number4 = (4+3)*7;
+console.log(number1);
+
+
+// - * /
