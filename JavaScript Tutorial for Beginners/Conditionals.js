0bf5a9ce27bfb426f4f055ce53203f632f923027
panw3i <panw3i@gmail.com>
2017-01-22 18:25:33 +0800

    loops

---
+++ JavaScript Tutorial for Beginners/Conditionals.js
@@ -0,0 +1,12 @@
+/**
+ * Created by pan on 2017/1/22.
+ */
+var food = "apple";
+if(food === "apple"){
+    console.log("We ate some fruit and spcifically an Apple");
+}else if(food === "meat"){
+    console.log("We did not eat fruit");
+}else{
+    console.log("we eat something else");
+}
+
--- JavaScript Tutorial for Beginners/loops.js
+++ JavaScript Tutorial for Beginners/loops.js
@@ -1,3 +1,10 @@
 /**
  * Created by pan on 2017/1/22.
  */
+
+var i =0;
+
+while(i<5){
+    console.log("A statement has run");
+    i++;
+}
