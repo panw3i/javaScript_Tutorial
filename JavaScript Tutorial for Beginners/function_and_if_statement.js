67d2528c3eefd3c98609a8b0dea82622d04b8db9
panw3i <panw3i@gmail.com>
2017-01-26 20:44:29 +0800

    loops

---
+++ JavaScript Tutorial for Beginners/function_and_if_statement.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pan on 2017/1/22.
+ */
+function batting(player, distance) {
+    if (distance <= 350) {
+        console.log(player + "  hit the bail")
+    }
+
+}
+
+batting("pan", 120);
